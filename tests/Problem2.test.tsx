423ab64da06d815b4566ffc3fb58adbd9ba31080
shankar_b <shankarsher123@gmail.com>
2024-12-02 13:05:20 -0600

    Additional test cases

--- tests/Problem2.test.tsx
+++ tests/Problem2.test.tsx
@@ -56,4 +56,60 @@ describe("Problem2: useMemo", () => {
         expect(calculateFactorial).toHaveBeenCalledWith(15);
         expect(calculateFactorial).toHaveReturnedWith(1307674368000);
     });
+
+    test("(Additional test case 1) Factorial not computed when re-rendering without changing value", async () => {
+        const onRender = vi.fn();
+        vi.mock("../src/utils/factorial", { spy: true });
+
+        render(
+            <Profiler id="AppProfiler" onRender={onRender}>
+                <App />
+            </Profiler>,
+        );
+
+        // Initial render
+        expect(onRender).toHaveBeenCalledTimes(1);
+        expect(calculateFactorial).toHaveBeenCalledTimes(4);
+
+        const renderButton = screen.getByRole("button");
+
+        // Click calculate
+        fireEvent.click(renderButton);
+        expect(onRender).toHaveBeenCalledTimes(2);
+        expect(calculateFactorial).toHaveBeenCalledTimes(4);
+
+        // Click calculate again
+        fireEvent.click(renderButton);
+        expect(onRender).toHaveBeenCalledTimes(3);
+        expect(calculateFactorial).toHaveBeenCalledTimes(4);
+    });
+
+    test("(Additional test case 2) Factorial computed correctly for different values", async () => {
+        const onRender = vi.fn();
+        vi.mock("../src/utils/factorial", { spy: true });
+
+        render(
+            <Profiler id="AppProfiler" onRender={onRender}>
+                <App />
+            </Profiler>,
+        );
+
+        const numberInput = screen.getByLabelText(
+            /Enter a number/i,
+        ) as HTMLInputElement;
+
+        // Update input value to 5
+        fireEvent.change(numberInput, { target: { value: "5" } });
+        expect(onRender).toHaveBeenCalledTimes(2);
+        expect(calculateFactorial).toHaveBeenCalledTimes(6);
+        expect(calculateFactorial).toHaveBeenCalledWith(5);
+        expect(calculateFactorial).toHaveReturnedWith(120);
+
+        // Update input value to 7
+        fireEvent.change(numberInput, { target: { value: "7" } });
+        expect(onRender).toHaveBeenCalledTimes(3);
+        expect(calculateFactorial).toHaveBeenCalledTimes(7);
+        expect(calculateFactorial).toHaveBeenCalledWith(7);
+        expect(calculateFactorial).toHaveReturnedWith(5040);
+    });
 });
